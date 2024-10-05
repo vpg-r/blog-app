7f8d9b94bd3814bdb3f6a16e45d8377b637367a0
Apple <apple@appurunoMacBook-Pro.local>
2024-10-05 18:14:19 +0900

    added appwrite auth and conf files

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf={
+    appwriteUrl:String(process.env.REACT_APP_APPRWRITE_URL),
+    appwriteProjectId:String(process.env.REACT_APP_APPRWRITE_PROJECT_ID),
+    appwriteDatabaseId:String(process.env.REACT_APP_DATABASE_ID),
+    appwriteCollectionId:String(process.env.REACT_APP_COLLECTION_ID),
+    appwriteBucketId:String(process.env.REACT_APP_BUCKET_ID),
+
+}
+
+export default conf
