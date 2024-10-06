307c4fdc573362dd2ba7e5e7a55b7d4a9018867c
Apple <apple@appurunoMacBook-Pro.local>
2024-10-06 21:40:28 +0900

    parsing html in react components

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import parse from "html-react-parser";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import appwriteService from "../appwrite/config";
+import Button from "../components/Button";
+import Container from "../components/container/Container";
+import { useSelector } from "react-redux";
 
 function Post() {
-  return (
-    <div>Post</div>
-  )
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+  const isAuthor = post && userData ? post.userId === userData.$id : false;
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        } else {
+          navigate("/");
+        }
+      });
+    }
+  });
+  const deletePost = () => {
+    appwriteService.deleteFile(post.$id).then((status) => {
+      if (status) {
+        appwriteService.deleteFile(post.featuredImg);
+      }
+      navigate("/");
+    });
+  };
+
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
+          <img
+            src={appwriteService.getFilePreview(post.featuredImg)}
+            alt={post.title}
+            className="rounded-xl"
+          />
+          {isAuthor && (
+            <div className="absolute-right-6 top-6">
+              <Link to={`/edit-post/${post.$id}`}>
+                <Button bgColor="bg-green-500" className="mr-3">
+                  Edit
+                </Button>
+              </Link>
+              <Button bgColor="bg-red-500" onClick={deletePost}>
+                Delete
+              </Button>
+            </div>
+          )}
+        </div>
+        <div className="w-full mb-6">
+          <h1 className="text-2xl font-bold">{post.title}</h1>
+          <div className="browser-css">{parse(post.content)}</div>
+        </div>
+      </Container>
+    </div>
+  ) : null;
 }
 
-export default Post
+export default Post;
